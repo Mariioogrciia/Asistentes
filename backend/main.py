f590387cc8e9884f98c7082c00b1c65b582b3f9c
Mario García <mario.meca2005@gmail.com>
2026-04-25 19:11:15 +0200

    responsive

--- backend/main.py
+++ backend/main.py
@@ -41,6 +41,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "https://asistentes-xcoc.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
