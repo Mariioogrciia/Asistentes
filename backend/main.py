28a48228894cb53601ebf39764363e13bf0d0c2b
Mario García <mario.meca2005@gmail.com>
2026-04-25 19:19:59 +0200

    cors

--- backend/main.py
+++ backend/main.py
@@ -39,10 +39,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost:8000",
-        "http://127.0.0.1:8000",
-        "https://asistentes-xcoc.vercel.app/",
+        "https://asistentes-xcoc.vercel.app",
     ],
+    allow_origin_regex=r"https://asistentes-.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
